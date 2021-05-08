903bd58529e7d98e47e0f1873b1ef0b1cd6ee2df
slfrucht <sfrucht@lmi.net>
2021-05-08 14:13:18 -0700

    add content to contact page

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -4,9 +4,39 @@ import { Link } from 'react-router-dom';
 
 function Contact() {
     return (
-        <div>
-            <h1>Contact us page</h1>
+        <div className="row contact-card">
+            <div className="col-12">
+            <Card bg="Light" border="dark">
+                <CardBody>
+                    <div className="col info-link">
+					<p>
+						To see other artwork of mine please visit: <br /> <a
+							href="https://www.behance.net/sarafrucht">Sara Frucht on
+							Behance</a>
+					</p>
+				</div>
+
+
+				<div className="info-link">
+					<p>
+						To find out about my artwork at Makers Paradise Art Gallery in
+						Berkeley, CA please visit: <br /> <a
+							href="https://www.makersparadise.org/">Makers Paradise Art Gallery</a>
+					</p>
+				</div>
+				<div className="info-link">
+					<p>
+						I would love to hear your feedback! <br /> <a
+							href="mailto:sfrucht@lmi.net?subject=Mail from SF.com">Email
+							me</a>
+					</p>
+				</div>
+                </CardBody>
+            </Card>
+
+            </div>
         </div>
+
     )
 }
 export default Contact;
